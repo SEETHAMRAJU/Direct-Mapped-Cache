4d6d0b177b311d4b9403b16800e692de8a463002
Byte-Rider <32098569+SEETHAMRAJU@users.noreply.github.com>
2018-11-26 09:57:11 +0530

    Added file handling (Sailesh)

--- Cache.v
+++ Cache.v
@@ -1,16 +1,17 @@
-module Cache(clk, Address, Data_Out, Hit_Miss);
+module Cache(clk, Address, Data_Out, Hit_Miss,rate);
 	input wire [31:0] Address; 			//the input address that is given
 	input clk; 					//clk for syncheonization
 	output reg [31:0] Data_Out; 			//for the data that will be returned from cache
 	output reg Hit_Miss; 				//set if there is a hit else 0 for miss
-    
-    	reg [15:0] [31:0] Cache_Memory[255:0]; 		//a memory of registers 256 x 16 words ,where 1 word is 32 bits.
+    	output reg [31:0]rate;
+   	
+	reg [15:0] [31:0] Cache_Memory[255:0]; 		//a memory of registers 256 x 16 words ,where 1 word is 32 bits.
     	reg [17:0] Cache_Tags[255:0]; 			//a set of registers for the tag bits
-	reg [7:0] memory[0:32763]; 			// the main memory
-
+	//reg [7:0] memory[0:32767]; 			// the main memory
+	reg [7:0]memory[0:8388608];
 	//set of registers used for storing parts of the addess like tag bits,
 	// index bits etc. for ease of working.
-    	reg [17:0] Tag_Bits; 				 
+	reg [17:0] Tag_Bits; 				 
     	reg [7:0] Index_Bits; 				
     	reg [3:0] Byte_Offset; 			
     	reg [31:0] M; 
@@ -26,6 +27,7 @@ module Cache(clk, Address, Data_Out, Hit_Miss);
 	initial begin
 		$readmemb("memory.list",memory);	
 		count = 0;
+		rate = 32'b0;
 	end
 	//Here in this block we seperate the address to tag bits , index bits
 	//etc. and then check for the address in the cache .Incase of a miss
@@ -37,7 +39,7 @@ module Cache(clk, Address, Data_Out, Hit_Miss);
 	        Byte_Offset = Address[5:2];
 		count+=1;
     		M = 32'b0000_0000_0000_0000_0000_0000_0000_1111;
-        	if(Cache_Tags[Index_Bits] == Tag_Bits)
+		if(Cache_Tags[Index_Bits] == Tag_Bits)
         	begin
 			Hit_Miss = 1;
         	    	Data_Out = Cache_Memory[Index_Bits][Byte_Offset];
@@ -46,6 +48,7 @@ module Cache(clk, Address, Data_Out, Hit_Miss);
         	begin
             		Hit_Miss = 0;
         	end
+		//$display("<->%b %d %h",Hit_Miss,Data_Out,Address);
     	end
     	//This is the code for getting the block of data if there is a cache miss. 
     	//here we are anding with the a given numebr to make the last 6 bits
@@ -54,6 +57,7 @@ module Cache(clk, Address, Data_Out, Hit_Miss);
     	begin
 		if(Hit_Miss == 0)
 		begin
+			rate = rate + 1;
 			count+=1;
 			temp3 = Address & 32'b11111111_11111111_11111111_11000000;
 			for(i=0;i<16;i++)
--- Cache_tb.v
+++ Cache_tb.v
@@ -3,28 +3,60 @@ module cache_tb;
 	reg [31:0]Address;
 	wire [31:0]Data_Out;
 	wire Hit_Miss;
+	//temporary variables used  for the file handling
+	integer fileo,filec,r,r1,r2;
+	integer count =0;
+	wire [31:0]rate;
+	reg [20:0]str;
+	reg [7:0]line;
 	Cache uut(
 		.clk(clk),
 		.Data_Out(Data_Out),
 		.Address(Address),
-		.Hit_Miss(Hit_Miss)
+		.Hit_Miss(Hit_Miss),
+		.rate(rate)
 	);
 	initial begin
 		$dumpfile("cache.vcd");
 		$dumpvars(0,cache_tb);
-		clk = 0;
-		$display("Direct Mapped Cache");
-		$display("---------------------------------------------------------------------\n");
-		Address = 32'b0000_0000_0000_0000_0000_0000_0000_1000;
-		#30;
-		$display("Hit=%b Data=%h address=%h\n",Hit_Miss,Data_Out,Address);
-		#5;
-		Address = 32'b0000_0000_0000_0000_0000_0000_0000_1100;
-		#5;
-		$display("Hit=%b Data=%h address=%h\n",Hit_Miss,Data_Out,Address);
+		clk = 0;Address = 32'b0;#30; 		//Initializing and initial delay
+		//file handling for address request
+		fileo = $fopenr("gcc2.trace");
+		if(!fileo)
+			$display("ERROR : CANNOT OPEN FILE\n");
+		else
+		begin
+			while(!$feof(fileo))
+			begin
+				if($fgets(line,fileo))
+				begin
+					if(count<2)
+						count+=1;
+					else if(count<10)
+					begin
+						if(line<97)
+						begin
+							Address[(4*(10-count))-1-:4] = (line-48);
+							count += 1;
+						end
+						else
+						begin
+							Address[(4*(10-count))-1-:4] = (line-87);
+							count += 1;
+						end
 
+					end
+					else
+					begin
+//						$display("%d %d %h",Hit_Miss,Data_Out,Address);
+						#5;
+						count = 0;
+					end
+				end
+			end
+		end
 	end
 	initial begin
-	repeat(50)#5 clk = ~clk;
-end
+	repeat(100)#5 clk = ~clk;
+	end
 endmodule
