421b147c33a078076c8ea53e71b6207e8fd092e9
Byte-Rider <32098569+SEETHAMRAJU@users.noreply.github.com>
2018-12-02 19:20:03 +0530

    Final Commit

--- generate.cpp
+++ generate.cpp
@@ -1,25 +1,39 @@
 #include<bits/stdc++.h>
+#define llint long long int
 #define append push_back
 #define SIZE 1000000
 using namespace std;
+string solve(llint x,llint s)
+{
+	string ans = "";
+	llint i=0;
+	while(i<s)
+		{x = x >> 1;i++;}
+	for(;i<s+8;i++)
+	{
+		if(x%2 == 0)
+			ans += "0";
+		else
+			ans += "1";
+		x = x>>1;
+	}
+	reverse(ans.begin(),ans.end());
+	return ans;
+
+}
 int main()
 {
 	freopen("memory.list","w",stdout);
 	unsigned long long int p=0;	
-	for(p;p<256;p++)
+	for(p;p<2097152;p++)
 	{
-		bitset<8> x(p);
-		for(int j=0;j<4;j++)
+		for(int j=24;j>=0;j-=8)
 		{
-			if(j == 3)
-				cout << x << endl;
-			else
-				cout << "00000000"<<endl; 
+			cout << solve(p,j) << endl;
 		}
 	}
 	return 0;
 }
-
 
 
 
